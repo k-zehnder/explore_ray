a744d40482a3984068867d29d78d8e3167031845
kevin <righttrianglesrkewl3@gmail.com>
2021-12-11 14:27:36 -0800

    updated readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ### Example of speed improvement on simple task. 
 ![much_faster!](https://github.com/k-zehnder/explore_ray/blob/main/images/ray_proof_speed.png)
+
+![code!]https://github.com/k-zehnder/explore_ray/blob/main/images/example_code.png
--- main.py
+++ main.py
@@ -9,33 +9,33 @@ def percentage_change(current, previous):
         return "undefined"
 
 if __name__ == "__main__":
-    # setup "analyzers"
-    sim_analyzer = SimilarityAnalyzer()
-    bw_analyzer= BlackWhiteThresholdAnalyzer()
-
-    # collect image data with ray
+    # collect image data *without* ray
     start = time.perf_counter()
     scraper = Scraper("/home/batman/Desktop/explore_ray/chromedriver", headless=True)
     for i in range(10):
         scraper.load_images_from_folder()
-    # results = scraper.get_images()
-    # print(len(results))
     default_duration = time.perf_counter() - start
     print(f'NO RAY: {default_duration * 1000:.1f}ms')
 
     
-    # collect image data with ray
+    # collect image data *with* ray
     ray.init()
     start = time.perf_counter()
     scraper = Ray_Scraper.remote("/home/batman/Desktop/explore_ray/chromedriver", headless=True)
     for i in range(10):
         scraper.load_images_from_folder.remote()
-    # results = ray.get(scraper.get_images.remote())
-    # print(len(results))
     ray_duration = time.perf_counter() - start
     print(f'WITH RAY: {ray_duration * 1000:.1f}ms')
     ray.shutdown()
     print(f'RAY is {percentage_change(default_duration, ray_duration)}% faster.')
+
+    # setup "analyzers"
+    sim_analyzer = SimilarityAnalyzer()
+    bw_analyzer= BlackWhiteThresholdAnalyzer()
+
+    # results = ray.get(scraper.get_images.remote())
+    # print(len(results))
+
     # img_urls = scraper.fetch_image_urls("cat", 5, 1)
     # TODO: scraper.persist_images(image_urls)
     
